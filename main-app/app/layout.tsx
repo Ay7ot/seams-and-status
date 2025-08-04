c9dffc3bda3858da7c3f1bc70b68124cf0dfebc7
Ay7ot <ayomidotzee@gmail.com>
2025-08-04 22:53:50 +0100

    fixed android and ios screen sizes

--- main-app/app/layout.tsx
+++ main-app/app/layout.tsx
@@ -72,8 +72,6 @@ export default function RootLayout({
                 <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
                 <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
                 <meta name="mobile-web-app-capable" content="yes" />
-                <meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: light)" />
-                <meta name="theme-color" content="#000000" media="(prefers-color-scheme: dark)" />
             </head>
             <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" sizes="180x180" />
             <body>
