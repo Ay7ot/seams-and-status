37a443ea95dfdc299d46ee7cf85473f93d1fefbc
Ay7ot <ayomidotzee@gmail.com>
2025-08-22 14:03:33 +0100

    Fixed service worker middleware

--- main-app/middleware.ts
+++ main-app/middleware.ts
@@ -32,7 +32,10 @@ export const config = {
          * - _next/static (static files)
          * - _next/image (image optimization files)
          * - favicon.ico (favicon file)
+         * - sw.js (service worker)
+         * - manifest.json (PWA manifest)
+         * - icons (PWA icons)
          */
-        '/((?!api|_next/static|_next/image|favicon.ico).*)',
+        '/((?!api|_next/static|_next/image|favicon.ico|sw.js|manifest.json|icons).*)',
     ],
 } 
